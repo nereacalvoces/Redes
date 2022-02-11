a2e7bdcbf6eb60d9881341a5f47091d9e07b26d2
xoel <xoel.diaz@udc.es>
2022-02-11 13:53:23 +0100

    Programa Funcional falta generalizacion

--- src/es/udc/redes/tutorial/copy/Copy.java
+++ src/es/udc/redes/tutorial/copy/Copy.java
@@ -1,9 +1,23 @@
 package es.udc.redes.tutorial.copy;
+import java.io.*;
 
 public class Copy {
+    public static void main(String[] args) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
 
-    public static void main(String[] args) {
+        try {
+            in = new FileInputStream("udc.gif");
+            out = new FileOutputStream("gifVacio.gif");
+            int c;
 
+            while((c = in.read()) != -1)
+                    out.write(c);
+        } finally {
+            if (in != null)
+                    in.close();
+            if (out != null)
+                    out.close();
+        }
     }
-    
 }
