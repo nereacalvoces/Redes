1be1f3bd76b8f2f464b783a33a5cef85f85f1758
xoel <xoel.diaz@udc.es>
2022-02-14 23:51:00 +0100

    algunos cambios (no funcional)

--- src/es/udc/redes/tutorial/tcp/server/ServerThread.java
+++ src/es/udc/redes/tutorial/tcp/server/ServerThread.java
@@ -1,6 +1,7 @@
 package es.udc.redes.tutorial.tcp.server;
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 /** Thread that processes an echo server connection. */
 
@@ -10,22 +11,44 @@ public class ServerThread extends Thread {
 
   public ServerThread(Socket s) {
     // Store the socket s
+    this.socket = s;
   }
 
   public void run() {
     try {
       // Set the input channel
-      // Set the output channel 
+      BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      // Set the output channel
+      PrintWriter output = new PrintWriter(socket.getOutputStream());
       // Receive the message from the client
-      // Sent the echo message to the client
+      Scanner sc = new Scanner(System.in);
+      String line = null;
+      while(!"exit".equalsIgnoreCase(line)) {
+        line = sc.nextLine();
+        output.println(line);
+        output.flush();
+        // Sent the echo message to the client
+        System.out.println("SERVER: Sending " + line +
+                " to " + socket.getInetAddress().toString() +
+                ":" + socket.getPort());
+      }
       // Close the streams
+        input.close();
+        output.close();
+        sc.close();
     // Uncomment next catch clause after implementing the logic
-    // } catch (SocketTimeoutException e) {
-    //  System.err.println("Nothing received in 300 secs");
+     } catch (SocketTimeoutException e) {
+     System.err.println("Nothing received in 300 secs");
     } catch (Exception e) {
       System.err.println("Error: " + e.getMessage());
-      } finally {
+    }
+    finally {
 	// Close the socket
+      try {
+        socket.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
--- src/es/udc/redes/tutorial/tcp/server/TcpServer.java
+++ src/es/udc/redes/tutorial/tcp/server/TcpServer.java
@@ -1,31 +1,39 @@
 package es.udc.redes.tutorial.tcp.server;
+import java.io.IOException;
 import java.net.*;
 
 /** Multithread TCP echo server. */
 
 public class TcpServer {
 
-  public static void main(String argv[]) {
+  public static void main(String argv[]) throws IOException {
     if (argv.length != 1) {
       System.err.println("Format: es.udc.redes.tutorial.tcp.server.TcpServer <port>");
       System.exit(-1);
     }
+    ServerSocket socket = null;
     try {
       // Create a server socket
+      socket = new ServerSocket(Integer.parseInt(argv[0]));
       // Set a timeout of 300 secs
+      socket.setSoTimeout(300000);
       while (true) {
         // Wait for connections
+        Socket socket1 = socket.accept();
         // Create a ServerThread object, with the new connection as parameter
+        ServerThread servidor = new ServerThread(socket1);
         // Initiate thread using the start() method
+        servidor.start();
       }
     // Uncomment next catch clause after implementing the logic
-    // } catch (SocketTimeoutException e) {
-    //  System.err.println("Nothing received in 300 secs");
+     } catch (SocketTimeoutException e) {
+      System.err.println("Nothing received in 300 secs");
     } catch (Exception e) {
       System.err.println("Error: " + e.getMessage());
       e.printStackTrace();
      } finally{
 	    //Close the socket
+          socket.close();
     }
   }
 }
