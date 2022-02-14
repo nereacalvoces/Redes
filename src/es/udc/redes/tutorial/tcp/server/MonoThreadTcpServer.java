6af1d597e7954ddc639cabc263693bcb18464fa1
xoel <xoel.diaz@udc.es>
2022-02-14 21:39:21 +0100

    Server funcional (?solo en el IDE¿)

--- src/es/udc/redes/tutorial/tcp/server/MonoThreadTcpServer.java
+++ src/es/udc/redes/tutorial/tcp/server/MonoThreadTcpServer.java
@@ -8,37 +8,49 @@ import java.io.*;
  */
 public class MonoThreadTcpServer {
 
-    public static void main(String argv[]) {
+    public static void main(String argv[]) throws IOException {
         if (argv.length != 1) {
             System.err.println("Format: es.udc.redes.tutorial.tcp.server.MonoThreadTcpServer <port>");
             System.exit(-1);
         }
+        ServerSocket socket = null;
         try {
             // Create a server socket
-            ServerSocket socket = new ServerSocket(Integer.parseInt(argv[0]));
+             socket = new ServerSocket(Integer.parseInt(argv[0]));
             // Set a timeout of 300 secs
             socket.setSoTimeout(300000);
             while (true) {
                 // Wait for connections
                 Socket socket1 = socket.accept();
                 // Set the input channel
-                InputStream input = socket1.getInputStream();
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket1.getInputStream()));
                 // Set the output channel
-                OutputStream output = socket1.getOutputStream();
+                PrintWriter output = new PrintWriter(socket1.getOutputStream(),true);
                 // Receive the client message
-                
+                String msg = input.readLine();
+                output.println(msg);
+                System.out.println("SERVER: Received " + (msg) +
+                                    " from " + socket1.getInetAddress().toString() +
+                                    ":" + socket1.getPort());
                 // Send response to the client
-
+                output.println(msg);
+                System.out.println("SERVER: Sending " + (msg) +
+                                    " to " + socket1.getInetAddress().toString() +
+                                    ":" + socket1.getPort());
                 // Close the streams
+                input.close();
+                output.close();
+                socket1.close();
             }
         // Uncomment next catch clause after implementing the logic            
-        //} catch (SocketTimeoutException e) {
-        //    System.err.println("Nothing received in 300 secs ");
+        } catch (SocketTimeoutException e) {
+          System.err.println("Nothing received in 300 secs ");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
             e.printStackTrace();
         } finally {
 	        //Close the socket
+            socket.close();
         }
     }
 }
