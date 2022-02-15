5fa22dda6b3e22450e044c9f1e3317f176f5029a
xoel <xoel.diaz@udc.es>
2022-02-15 13:02:55 +0100

    copy

--- src/es/udc/redes/tutorial/copy/Copy.java
+++ src/es/udc/redes/tutorial/copy/Copy.java
@@ -3,21 +3,21 @@ import java.io.*;
 
 public class Copy {
     public static void main(String[] args) throws IOException {
-        FileInputStream in = null;
-        FileOutputStream out = null;
+        FileInputStream input = null;
+        FileOutputStream output = null;
 
         try {
-            in = new FileInputStream(args[0]);
-            out = new FileOutputStream(args[1]);
+            input = new FileInputStream(args[0]);
+            output = new FileOutputStream(args[1]);
             int c;
 
-            while((c = in.read()) != -1)
-                    out.write(c);
+            while((c = input.read()) != -1)
+                    output.write(c);
         } finally {
-            if (in != null)
-                    in.close();
-            if (out != null)
-                    out.close();
+            if (input != null)
+                    input.close();
+            if (output != null)
+                    output.close();
         }
     }
 }
