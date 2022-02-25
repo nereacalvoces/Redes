e6a37ffd968312285f6629caadc6963a56b75268
xoel <xoel.diaz@udc.es>
2022-02-25 13:37:35 +0100

    Copypasted Tcp

--- src/es/udc/redes/webserver/ServerThread.java
+++ src/es/udc/redes/webserver/ServerThread.java
@@ -2,6 +2,7 @@ package es.udc.redes.webserver;
 
 import java.net.*;
 import java.io.*;
+import java.nio.Buffer;
 
 
 public class ServerThread extends Thread {
@@ -17,14 +18,36 @@ public class ServerThread extends Thread {
         try {
           // This code processes HTTP requests and generates 
           // HTTP responses
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
+            String request = input.readLine();
+            String[] requestParam = request.split("");
+            String path = requestParam[1];
+            File file = new File(path);
+            if(!file.exists()){
+                output.write("HTTP 404");
+            }
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line=br.readLine())!=null)
+                output.write(line);
+            br.close();
+            fr.close();
+            output.close();
           // Uncomment next catch clause after implementing the logic
-          //
-        // } catch (SocketTimeoutException e) {
-        //    System.err.println("Nothing received in 300 secs");
+
+         } catch (SocketTimeoutException e) {
+            System.err.println("Nothing received in 300 secs");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         } finally {
             // Close the client socket
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/es/udc/redes/webserver/WebServer.java
+++ src/es/udc/redes/webserver/WebServer.java
@@ -1,9 +1,34 @@
 package es.udc.redes.webserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
 public class WebServer {
     
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Format: es.udc.redes.tutorial.tcp.server.TcpServer <port>");
+            System.exit(-1);
+        }
+        ServerSocket serverSocket = null;
+        try{
+            serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            serverSocket.setSoTimeout(3000000);
+            while(true){
+                Socket socket = serverSocket.accept();
+                ServerThread server = new ServerThread(socket);
+                server.start();
+            }
+        } catch (SocketTimeoutException e) {
+            System.err.println("Nothing received in 300 secs");
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            serverSocket.close();
+        }
     }
-    
 }
