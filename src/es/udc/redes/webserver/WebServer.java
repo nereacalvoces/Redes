450fde90e57ce93b2c20d1724a5ace45aa306f5b
xoel <xoel.diaz@udc.es>
2022-02-28 20:26:52 +0100

    Primera parte lista (String troceado para recibir archivo e imprimirlo

--- src/es/udc/redes/webserver/WebServer.java
+++ src/es/udc/redes/webserver/WebServer.java
@@ -1,10 +1,12 @@
 package es.udc.redes.webserver;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.nio.Buffer;
+import java.nio.charset.StandardCharsets;
 
 public class WebServer {
     
@@ -17,10 +19,33 @@ public class WebServer {
         try{
             serverSocket = new ServerSocket(Integer.parseInt(args[0]));
             serverSocket.setSoTimeout(3000000);
-            while(true){
-                Socket socket = serverSocket.accept();
-                ServerThread server = new ServerThread(socket);
-                server.start();
+            System.out.println("Server started, listening for messages.");
+            while(true) {
+                Socket client = serverSocket.accept();
+                System.out.println("Got new message\n");
+                InputStreamReader inputStreamReader = new InputStreamReader(client.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder requestBuilder = new StringBuilder();
+                String line;
+                line = bufferedReader.readLine();
+                while (!line.isBlank()) {
+                    requestBuilder.append(line).append("\r\n");
+                    line = bufferedReader.readLine();
+                }
+                System.out.println("--REQUEST--");
+                System.out.println(requestBuilder);
+                String[] parts = requestBuilder.toString().split(" ");
+                StringBuilder stringBuilder = new StringBuilder(parts[1]);
+                stringBuilder.deleteCharAt(0);
+                FileInputStream input = new FileInputStream(stringBuilder.toString());
+                int c;
+                    OutputStream clientOutput = client.getOutputStream();
+                    clientOutput.write(("HTTP/1.1 200 OK \r\n").getBytes());
+                    clientOutput.write(("\r\n").getBytes());
+                while ((c = input.read()) != -1)
+                    clientOutput.write(c);
+                clientOutput.flush();
+                client.close();
             }
         } catch (SocketTimeoutException e) {
             System.err.println("Nothing received in 300 secs");
