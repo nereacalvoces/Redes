9aa7fc120d961e2e1dd7d55c3ce4f8cf049c438e
xoel <xoel.diaz@udc.es>
2022-03-02 12:38:20 +0100

    Added '.' to the beggining of string to path

--- src/es/udc/redes/webserver/WebServer.java
+++ src/es/udc/redes/webserver/WebServer.java
@@ -34,9 +34,7 @@ public class WebServer {
                 System.out.println(requestBuilder);
                 String[] parts = requestBuilder.toString().split(" ");
                 StringBuilder stringBuilder = new StringBuilder(parts[1]);
-                if(stringBuilder.charAt(0) == '/') {
-                    stringBuilder.deleteCharAt(0);
-                }
+                stringBuilder.insert(0,'.');
                 FileInputStream input = new FileInputStream(stringBuilder.toString());
                 int c;
                     OutputStream clientOutput = client.getOutputStream();
