8c2bd0044f40c7ac256f3f9873a121df21fc3483
xoel <xoel.diaz@udc.es>
2022-03-02 19:02:56 +0100

    ServerThread ?funcional¿ con Date y Server (WebServer no funciona)

--- src/es/udc/redes/webserver/ServerThread.java
+++ src/es/udc/redes/webserver/ServerThread.java
@@ -2,7 +2,9 @@ package es.udc.redes.webserver;
 
 import java.net.*;
 import java.io.*;
-import java.nio.Buffer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class ServerThread extends Thread {
@@ -10,44 +12,80 @@ public class ServerThread extends Thread {
     private Socket socket;
 
     public ServerThread(Socket s) {
-        // Store the socket s
         this.socket = s;
     }
 
     public void run() {
         try {
-          // This code processes HTTP requests and generates 
-          // HTTP responses
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
-            String request = input.readLine();
-            String[] requestParam = request.split("");
-            String path = requestParam[1];
-            File file = new File(path);
-            if(!file.exists()){
-                output.write("HTTP 404");
-            }
-            FileReader fr = new FileReader(file);
-            BufferedReader br = new BufferedReader(fr);
-            String line;
-            while ((line=br.readLine())!=null)
-                output.write(line);
-            br.close();
-            fr.close();
+            String msg = input.readLine();
+            output.println(msg);
+            System.out.println("SERVER: Received " +(msg)+
+                    " from " + socket.getInetAddress().toString() +
+                    ":" + socket.getPort());
+            output.println(msg);
+            input.close();
             output.close();
-          // Uncomment next catch clause after implementing the logic
-
-         } catch (SocketTimeoutException e) {
+        } catch (SocketTimeoutException e) {
             System.err.println("Nothing received in 300 secs");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
-        } finally {
-            // Close the client socket
+        }
+        finally {
             try {
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Format: es.udc.redes.webserver.WebServer <port>");
+            System.exit(-1);
+        }
+        ServerSocket serverSocket = null;
+        try{
+            serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            serverSocket.setSoTimeout(3000000);
+            while(true) {
+                Socket client = serverSocket.accept();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                StringBuilder requestBuilder = new StringBuilder();
+                String line = bufferedReader.readLine();
+                while (!line.isBlank()) {
+                    requestBuilder.append(line).append("\r\n");
+                    line = bufferedReader.readLine();
+                }
+                System.out.println(requestBuilder);
+                String[] parts = requestBuilder.toString().split(" ");
+                StringBuilder stringBuilder = new StringBuilder(parts[1]);
+                if (stringBuilder.charAt(0) == '/')
+                    stringBuilder.insert(0,'.');
+                FileInputStream input = new FileInputStream(stringBuilder.toString());
+                int c;
+                OutputStream clientOutput = client.getOutputStream();
+                clientOutput.write(("HTTP/1.0 200 OK\r\n").getBytes());
+                DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z");
+                Date date = new Date();
+                clientOutput.write(("Date: "+dateFormat.format(date)+"\r\n").getBytes());
+                clientOutput.write(("Server: WebServer_695\r\n").getBytes());
+                clientOutput.write(("Content-Length: "+input.length+"\r\n").getBytes());
+                clientOutput.write(("\r\n").getBytes());
+
+                while ((c = input.read()) != -1)
+                    clientOutput.write(c);
+                clientOutput.flush();input.close();clientOutput.close();client.close();
+            }
+        } catch (SocketTimeoutException e) {
+            System.err.println("Nothing received in 300 secs");
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            serverSocket.close();
         }
     }
 }
--- src/es/udc/redes/webserver/WebServer.java
+++ src/es/udc/redes/webserver/WebServer.java
@@ -3,55 +3,30 @@ package es.udc.redes.webserver;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.net.SocketTimeoutException;
-import java.nio.Buffer;
-import java.nio.charset.StandardCharsets;
 
 public class WebServer {
-    
-    public static void main(String[] args) throws IOException {
-        if (args.length != 1) {
+    public static void main(String[] argv) throws IOException {
+        if (argv.length != 1) {
             System.err.println("Format: es.udc.redes.tutorial.tcp.server.TcpServer <port>");
             System.exit(-1);
         }
-        ServerSocket serverSocket = null;
-        try{
-            serverSocket = new ServerSocket(Integer.parseInt(args[0]));
-            serverSocket.setSoTimeout(3000000);
-            System.out.println("Server started, listening for messages.");
-            while(true) {
-                Socket client = serverSocket.accept();
-                InputStreamReader inputStreamReader = new InputStreamReader(client.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                StringBuilder requestBuilder = new StringBuilder();
-                String line = bufferedReader.readLine();
-                while (!line.isBlank()) {
-                    requestBuilder.append(line).append("\r\n");
-                    line = bufferedReader.readLine();
-                }
-                System.out.println("--REQUEST--");
-                System.out.println(requestBuilder);
-                String[] parts = requestBuilder.toString().split(" ");
-                StringBuilder stringBuilder = new StringBuilder(parts[1]);
-                stringBuilder.insert(0,'.');
-                FileInputStream input = new FileInputStream(stringBuilder.toString());
-                int c;
-                    OutputStream clientOutput = client.getOutputStream();
-                    clientOutput.write(("HTTP/1.1 200 OK \r\n").getBytes());
-                    clientOutput.write(("\r\n").getBytes());
-                while ((c = input.read()) != -1)
-                    clientOutput.write(c);
-                clientOutput.flush();
-                client.close();
+        ServerSocket socketServidor = null;
+        try {
+            socketServidor = new ServerSocket(Integer.parseInt(argv[0]));
+            socketServidor.setSoTimeout(300000);
+            while (true) {
+                Socket socket = socketServidor.accept();
+                ServerThread servidor = new ServerThread(socket);
+                servidor.start();
             }
         } catch (SocketTimeoutException e) {
             System.err.println("Nothing received in 300 secs");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
             e.printStackTrace();
-        } finally {
-            serverSocket.close();
+        } finally{
+            socketServidor.close();
         }
     }
 }
