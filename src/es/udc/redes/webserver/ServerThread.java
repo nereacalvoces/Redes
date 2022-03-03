b910521dfc697f684c842c073fa440f52bbdb334
xoel <xoel.diaz@udc.es>
2022-03-03 16:55:29 +0100

    Process Head request (unless NotFound and BadRequest)

--- src/es/udc/redes/webserver/ServerThread.java
+++ src/es/udc/redes/webserver/ServerThread.java
@@ -23,9 +23,7 @@ public class ServerThread extends Thread {
             StringBuilder file = new StringBuilder(parts[1]);
             if (file.charAt(0) == '/')
                 file.insert(0, '.');
-            System.out.println(file);
             FileInputStream input2 = new FileInputStream(file.toString());
-            System.out.println(input2.getChannel().size());
             if (parts[0].equals("GET"))
                 processGet(input2,file);
             if (parts[0].equals("HEAD"))
@@ -59,8 +57,18 @@ public class ServerThread extends Thread {
         clientOutput.flush();
         clientOutput.close();
     }
-    public void processHead(FileInputStream input, StringBuilder file) throws IOException{
-        
+    public void processHead(FileInputStream input, StringBuilder file) throws IOException {
+        DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z");
+        Date date = new Date();
+        OutputStream clientOutput = socket.getOutputStream();
+        clientOutput.write(("HTTP/1.0 200 OK\r\n").getBytes());
+        clientOutput.write(("Date: " + dateFormat.format(date) + "\r\n").getBytes());
+        clientOutput.write(("Server: WebServer_695\r\n").getBytes());
+        clientOutput.write(("Content-Length: "+input.getChannel().size()+"\r\n").getBytes());
+        selectContentType(file,clientOutput);
+        clientOutput.write(("\r\n").getBytes());
+        clientOutput.flush();
+        clientOutput.close();
     }
     public void selectContentType(StringBuilder file,OutputStream output) throws IOException {
         String[] particion = file.toString().split("\\.");
