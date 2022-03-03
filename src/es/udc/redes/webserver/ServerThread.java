dcb06a3859a076ba74826e3f0750484ef5ee1607
xoel <xoel.diaz@udc.es>
2022-03-03 14:11:04 +0100

    Content-Length añadido a headers y prototype de Content-type

--- src/es/udc/redes/webserver/ServerThread.java
+++ src/es/udc/redes/webserver/ServerThread.java
@@ -17,49 +17,37 @@ public class ServerThread extends Thread {
 
     public void run() {
         try {
+            DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z");
+            Date date = new Date();
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
             String msg = input.readLine();
-            System.out.println(msg);
-            while (true) {
-                //Socket client = serverSocket.accept();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                /*StringBuilder requestBuilder = new StringBuilder();
-                String line = bufferedReader.readLine();
-                while (!line.isBlank()) {
-                    requestBuilder.append(line).append("\r\n");
-                    line = input.readLine();
-                }*/
-                String[] parts = msg.split(" ");
-                StringBuilder stringBuilder = new StringBuilder(parts[1]);
-                System.out.println(stringBuilder);
-                if (stringBuilder.charAt(0) == '/')
-                    stringBuilder.insert(0, '.');
-                FileInputStream input2 = new FileInputStream(stringBuilder.toString());
-                int c;
-                OutputStream clientOutput = socket.getOutputStream();
-                clientOutput.write(("HTTP/1.0 200 OK\r\n").getBytes());
-                DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z");
-                Date date = new Date();
-                clientOutput.write(("Date: " + dateFormat.format(date) + "\r\n").getBytes());
-                clientOutput.write(("Server: WebServer_695\r\n").getBytes());
-                //clientOutput.write(("Content-Length: "+input.length+"\r\n").getBytes());
-                clientOutput.write(("\r\n").getBytes());
-
-                while ((c = input2.read()) != -1)
-                    clientOutput.write(c);
-                clientOutput.flush();
-                input.close();
-                clientOutput.close();
-                socket.close();
-            }
-        }
-         catch (SocketTimeoutException e) {
+            String[] parts = msg.split(" ");
+            StringBuilder file = new StringBuilder(parts[1]);
+            if (file.charAt(0) == '/')
+                file.insert(0, '.');
+            System.out.println(file);
+            FileInputStream input2 = new FileInputStream(file.toString());
+            System.out.println(input2.getChannel().size());
+            int c;
+            OutputStream clientOutput = socket.getOutputStream();
+            clientOutput.write(("HTTP/1.0 200 OK\r\n").getBytes());
+            clientOutput.write(("Date: " + dateFormat.format(date) + "\r\n").getBytes());
+            clientOutput.write(("Server: WebServer_695\r\n").getBytes());
+            clientOutput.write(("Content-Length: "+input2.getChannel().size()+"\r\n").getBytes());
+            //clientOutput.write(("Content-Type: "));
+            //splitear stringbuilder por el punto y dsps hacer if con la extension if parts [1] == gif tal ...
+            clientOutput.write(("\r\n").getBytes());
+            while ((c = input2.read()) != -1)
+                clientOutput.write(c);
+            clientOutput.flush();
+            clientOutput.close();
+        } catch (SocketTimeoutException e) {
             System.err.println("Nothing received in 300 secs");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
+        }
+        finally {
             try {
                 socket.close();
             } catch (IOException e) {
