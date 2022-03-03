ac43d64197f2f5c0f4709e1a5cc9e3125cabb49c
xoel <xoel.diaz@udc.es>
2022-03-03 16:51:22 +0100

    Procesa peticiones HEAD (excepto las NotFound y BadRequest)

--- src/es/udc/redes/webserver/ServerThread.java
+++ src/es/udc/redes/webserver/ServerThread.java
@@ -17,10 +17,7 @@ public class ServerThread extends Thread {
 
     public void run() {
         try {
-            DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z");
-            Date date = new Date();
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
             String msg = input.readLine();
             String[] parts = msg.split(" ");
             StringBuilder file = new StringBuilder(parts[1]);
@@ -29,19 +26,8 @@ public class ServerThread extends Thread {
             System.out.println(file);
             FileInputStream input2 = new FileInputStream(file.toString());
             System.out.println(input2.getChannel().size());
-            int c;
-            OutputStream clientOutput = socket.getOutputStream();
-            clientOutput.write(("HTTP/1.0 200 OK\r\n").getBytes());
-            clientOutput.write(("Date: " + dateFormat.format(date) + "\r\n").getBytes());
-            clientOutput.write(("Server: WebServer_695\r\n").getBytes());
-            clientOutput.write(("Content-Length: "+input2.getChannel().size()+"\r\n").getBytes());
-            //clientOutput.write(("Content-Type: "));
-            //splitear stringbuilder por el punto y dsps hacer if con la extension if parts [1] == gif tal ...
-            clientOutput.write(("\r\n").getBytes());
-            while ((c = input2.read()) != -1)
-                clientOutput.write(c);
-            clientOutput.flush();
-            clientOutput.close();
+            if (parts[0].equals("GET"))
+                processGet(input2,file);
         } catch (SocketTimeoutException e) {
             System.err.println("Nothing received in 300 secs");
         } catch (Exception e) {
@@ -53,6 +39,34 @@ public class ServerThread extends Thread {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+    public void processGet(FileInputStream input, StringBuilder file) throws IOException {
+        DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z");
+        Date date = new Date();
+        OutputStream clientOutput = socket.getOutputStream();
+        clientOutput.write(("HTTP/1.0 200 OK\r\n").getBytes());
+        clientOutput.write(("Date: " + dateFormat.format(date) + "\r\n").getBytes());
+        clientOutput.write(("Server: WebServer_695\r\n").getBytes());
+        clientOutput.write(("Content-Length: "+input.getChannel().size()+"\r\n").getBytes());
+        selectContentType(file,clientOutput);
+        clientOutput.write(("\r\n").getBytes());
+        int c;
+        while ((c = input.read()) != -1)
+            clientOutput.write(c);
+        clientOutput.flush();
+        clientOutput.close();
+    }
+
+    public void selectContentType(StringBuilder file,OutputStream output) throws IOException {
+        String[] particion = file.toString().split("\\.");
+        System.out.println(particion[2]);
+        switch (particion[2]) {
+            case "html" -> output.write(("Content-Type: text/html\r\n").getBytes());
+            case "txt" -> output.write(("Content-Type: text/plain\r\n").getBytes());
+            case "gif" -> output.write(("Content-Type: image/gif\r\n").getBytes());
+            case "png" -> output.write(("Content-Type: image/png\r\n").getBytes());
+            default -> output.write(("Content-Type: application/octet-stream\r\n").getBytes());
         }
     }
 }
